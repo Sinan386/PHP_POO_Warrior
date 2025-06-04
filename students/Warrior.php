df4eb09ee5692b42d6a73cdae4e9cca4ab0ceb2a
theoDS-campus <etack8r@gmail.com>
2025-06-04 15:33:47 +0200

    - 4 done

--- students/Warrior.php
+++ students/Warrior.php
@@ -4,4 +4,12 @@ require_once __DIR__ . "/../base/LocalWarrior.php";
 
 $GLOBALS['warriorName'] = 'SuperFort';
 
-abstract class Warrior extends LocalWarrior {}
+abstract class Warrior extends LocalWarrior {
+    public string $name;
+    public int $speed;
+    public int $life;
+    public int $shield;
+    public string $imageUrl;
+  
+
+}
