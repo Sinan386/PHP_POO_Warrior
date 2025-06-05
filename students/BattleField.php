f3725e6afd7f602020ca9b1d78bebcc00cb6c77d
theoDS-campus <etack8r@gmail.com>
2025-06-05 10:49:12 +0200

    - fin du projet

--- students/BattleField.php
+++ students/BattleField.php
@@ -9,7 +9,7 @@ class BattleField extends BaseBattleField
     public static function createMyWarrior() {
          global $warriorName;
 
-         $pikachu = new MarvelWarrior($warriorName);
+         $pikachu = new PokemonWarrior($warriorName);
         $pikachu->imageUrl = "https://www.pokepedia.fr/images/thumb/e/e7/Pikachu-RFVF.png/250px-Pikachu-RFVF.png";
 
         $eclair = new Weapon(1, 90);
@@ -24,5 +24,14 @@ class BattleField extends BaseBattleField
 
      public static function createOtherWarrior() {
         
+        $carapuce = new PokemonWarrior('carapuce');
+        $carapuce->imageUrl = "https://wiki.rpgdbz.com/sites/default/files/styles/table/public/2019-12/raw.gif?itok=O1pyNm1J";
+
+        $pistoletEau = new Weapon(1, 40);
+        $pistoletEau->imageUrl = "https://www.pokepedia.fr/images/thumb/c/c5/Pikachu_Tonnerre.png/800px-Pikachu_Tonnerre.png?20150322175103";
+
+        $carapuce->weapon = $pistoletEau;
+
+        $carapuce->saveNew();
     }
 }
